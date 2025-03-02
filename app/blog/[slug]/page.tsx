a980a0de82f4d7997388de71c2a575c4f3ab7328
Dibadan <dibadan98@gmail.com>
2025-03-02 12:25:27 +0100

    Initial Commit

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,35 +3,41 @@ import { BlogPostContent } from "@/components/blog/blog-post-content";
 import { fetchArticles, fetchArticleBySlug } from "@/sanity/sanity-query";
 import { BlogPostHeader } from "@/components/blog/blog-post-header";
 
+export async function generateStaticParams() {
+  const articles = await fetchArticles(); // Fetch all articles
+  return articles.map((post: any) => ({
+    slug: post.slug.current, // Correct slug structure
+  }));
+}
+
+
 export default async function BlogPostPage({
   params,
 }: {
-  params: { slug: string }; // Explicitly typing the params object
+  params: { slug: string };
 }) {
-  // Fetch the article by slug
   const post = await fetchArticleBySlug(params.slug);
-  console.log("POST = ", post)
 
   if (!post) {
-    notFound(); // Show a 404 page if the article doesn't exist
+    notFound(); // If the post doesn't exist, show 404
   }
 
   return (
     <div className="min-h-screen pt-20">
       <article className="py-20 px-4">
         <div className="max-w-4xl mx-auto">
-          {/* Blog Post Content */}
           <BlogPostContent post={{
-                      title: post.title,
-                      excerpt: post.excerpt,
-                      slug: post.slug.current,
-                      mainImage: post.mainImage?.asset?.url,
-                      author: post.author?.name,
-                      publishedAt: post.publishedAt,
-                      content: post.content
-                    }} />
+              title: post.title,
+              excerpt: post.excerpt,
+              slug: post.slug.current,
+              mainImage: post.mainImage?.asset?.url,
+              author: post.author?.name,
+              publishedAt: post.publishedAt,
+              content: post.content,
+            }} />
         </div>
       </article>
     </div>
   );
 }
+
