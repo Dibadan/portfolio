5e67478bcda6a881f90bdcacef480f80bb11c0f0
Dibadan <dibadan98@gmail.com>
2025-03-02 13:22:48 +0100

    Initial Commit

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { notFound } from "next/navigation";
 import { BlogPostContent } from "@/components/blog/blog-post-content";
 import { fetchArticles, fetchArticleBySlug } from "@/sanity/sanity-query";
-import { BlogPostHeader } from "@/components/blog/blog-post-header";
 
 export async function generateStaticParams() {
   const articles = await fetchArticles(); // Fetch all articles
@@ -10,12 +9,13 @@ export async function generateStaticParams() {
   }));
 }
 
+interface BlogPostParams {
+  params: {
+    slug: string;
+  };
+}
 
-export default async function BlogPostPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default async function BlogPostPage({ params }: BlogPostParams) {
   const post = await fetchArticleBySlug(params.slug);
 
   if (!post) {
@@ -26,7 +26,8 @@ export default async function BlogPostPage({
     <div className="min-h-screen pt-20">
       <article className="py-20 px-4">
         <div className="max-w-4xl mx-auto">
-          <BlogPostContent post={{
+          <BlogPostContent
+            post={{
               title: post.title,
               excerpt: post.excerpt,
               slug: post.slug.current,
@@ -34,10 +35,10 @@ export default async function BlogPostPage({
               author: post.author?.name,
               publishedAt: post.publishedAt,
               content: post.content,
-            }} />
+            }}
+          />
         </div>
       </article>
     </div>
   );
 }
-
